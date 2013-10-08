a303d911b65e512cd7d562ebaa88872c148e050e
jagannathan sundaram <rcjagan@gmail.com>
2013-10-08 19:58:39 +0530

    vertisinfo assignment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vertis::Application.config.secret_token = '541088dbe84d92c9de31904a2fc561f241535fde7ed2b9d8f025081893d87c2ffb4ef760e0b18bec3030512644a432956c28bc74d854d71e29b0b20dbc7ae8b7'
